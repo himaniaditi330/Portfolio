0c2c757cba0536800ac3c9024ac55db3ae868392
himaniaditi330 <41178581+himaniaditi330@users.noreply.github.com>
2021-03-25 16:33:28 +0530

    Add files via upload

---
+++ Web Pages/edu.php
@@ -0,0 +1,83 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Home</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+
+<!-- header-->
+<link type="text/css" rel="stylesheet" href="css/header.css">
+
+<!-- side-->
+<link type="text/css" rel="stylesheet" href="css/side.css">
+
+
+<!-- Footer -->
+<link type="text/css" rel="stylesheet" href="css/footer.css">
+
+
+
+
+</head>
+<body>
+
+<!-- Include header bar-->
+  <?php include 'header.php';?>
+
+
+<!-- Include navigation bar-->
+ <div class="navbar">
+ <a href="home.php" ><i class="fa fa-fw fa-home"></i>&nbsp Home</a>
+  <a href="reInt.php"><i class="fa fa-fw fa-book"></i>&nbsp Reseach Interests</a>
+  <a href="experience.php" ><i class='fa fa-fw fa-briefcase'></i>&nbsp Experience</a>
+  <a href="edu.php" class="active"><i class="fa fa-fw fa-graduation-cap"></i>&nbsp Education</a>
+  <a href="AP.php"><i class='fa fa-fw fa-trophy'></i>&nbsp Awards and Publications</a>
+  <a href="CR.php" ><i class='fa fa-fw fa-handshake-o'></i>&nbsp Conferences & Reviews</a>
+  <a href="talk.php"><i class='fa fa-fw fa-microphone'></i>&nbsp Talks</a>
+  <a href="contact.php"><i class='fa fa-fw fa-phone'></i>&nbsp Contact Details</a>
+  
+</div>
+ 
+
+
+<div class="row">
+
+<!-- Include side bar-->
+  <?php include 'side.php';?>
+  
+  <div class="main">
+  
+
+
+
+
+
+    <h2>EDUCATION</h2>
+    
+    <ul>
+		<li>
+			<p>M. Sc. (Mathematics) from Jadavpur University, Kolkata, INDIA .</p>
+		</li>
+		<li>
+			<p>B. Sc. (Mathematics (Honors)) from Jadavpur University, Kolkata, INDIA .</p>
+		</li>
+		<li>
+			<p>Diploma in Chinese Language from Barkatullah University, M. P. INDIA .</p>
+		</li>
+		<li>
+			<p>Interpreter-ship in Chinese Language from School of Foreign Languages, Delhi, INDIA.</p>
+		</li>
+	</ul>
+    <br>
+    
+  </div>
+</div>
+
+<!-- Include footer bar-->
+  <?php include 'footer.php';?>
+
+</body>
+</html>
---
+++ Web Pages/experience.php
@@ -0,0 +1,117 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+<title>Home</title>
+<meta charset="UTF-8">
+<meta name="viewport" content="width=device-width, initial-scale=1">
+<link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
+<!-- css -->
+<link rel="stylesheet" href="css/style.css" type="text/css" media="all" />
+
+<!-- header-->
+<link type="text/css" rel="stylesheet" href="css/header.css">
+
+<!-- side-->
+<link type="text/css" rel="stylesheet" href="css/side.css">
+
+
+<!-- Footer -->
+<link type="text/css" rel="stylesheet" href="css/footer.css">
+
+
+
+
+</head>
+<body>
+
+<!-- Include header bar-->
+  <?php include 'header.php';?>
+
+
+<!-- Include navigation bar-->
+ <div class="navbar">
+  <a href="home.php" ><i class="fa fa-fw fa-home"></i>&nbsp Home</a>
+  <a href="reInt.php" ><i class="fa fa-fw fa-book"></i>&nbsp Reseach Interests</a>
+  <a href="experience.php" class="active"><i class='fa fa-fw fa-briefcase'></i>&nbsp Experience</a>
+  <a href="edu.php"><i class="fa fa-fw fa-graduation-cap"></i>&nbsp Education</a>
+  <a href="AP.php"><i class='fa fa-fw fa-trophy'></i>&nbsp Awards and Publications</a>
+  <a href="CR.php" ><i class='fa fa-fw fa-handshake-o'></i>&nbsp Conferences & Reviews</a>
+  <a href="talk.php"><i class='fa fa-fw fa-microphone'></i>&nbsp Talks</a>
+  <a href="contact.php"><i class='fa fa-fw fa-phone'></i>&nbsp Contact Details</a>
+  
+</div>
+ 
+
+
+<div class="row">
+
+<!-- Include side bar-->
+  <?php include 'side.php';?>
+  
+  <div class="main">
+    <h2>EXPERIENCES</h2>
+    
+	<ul>
+		<li>
+			<p>Scientist in Defence R & D Organisation (DRDO), Delhi for more than 20 years</p>
+		</li>
+	</ul>
+	<br>
+	
+	<h2>MEMBER OF BOARD OF STUDIES IN MATHEMATICS</h2>
+    
+	<ul>
+		<li>
+			<p>Kavayitri Bahinabai Chaudhari North Maharashtra University from 12 Oct 2018 – 31 Aug 2022.</p>
+		</li>
+		<li>
+			<p>Moolji Jaitha College, North Maharashtra University from Oct 2018 till date.</p>
+		</li>
+		<li>
+			<p>National Institute of Science and Technology, Berhampur, Odisha from 30 Oct 2018.</p>
+		</li>
+	</ul>
+	<br>
+    
+
+	<h2>SRC MEMBER OF NORTH CAP UNIVERSITY, GURGAON</h2>
+    
+	<ul>
+		<li>
+			<p>Students pursuing PhD on Information Security of Department of Computer Science, North-Cap University, Gurgaon from 2014 to till date.</p>
+		</li>
+		
+	</ul>
+	<br>
+	
+
+	<h2>MEMBER OF EXECUTIVE COMMITTEE</h2>
+    
+	<ul>
+		<li>
+			<p>Cryptology Research Society of India (CRSI) from January 2015 – December 2016.</p>
+		</li>
+		
+	</ul>
+	<br>
+	
+
+
+	<div class="right">
+	<div class="pagination">
+   <a href="#">&laquo;</a>
+  <a href="experience.php" class="active">1</a>
+  <a href="experience1.php" >2</a>
+  <a href="experience2.php" >3</a>
+<a href="#">&raquo;</a>
+</div>
+</div>
+
+  </div>
+</div>
+
+<!-- Include footer bar-->
+  <?php include 'footer.php';?>
+
+</body>
+</html>
