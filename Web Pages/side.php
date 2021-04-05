2a095cc03d7ccb7f056c0e17bb2ab390b3d06ca0
himaniaditi330 <himaniaditi330@gmail.com>
2021-04-05 14:49:06 +0530

    Initial commit

--- Web Pages/side.php
+++
@@ -1,9 +0,0 @@
-<div class="side">
-    
-    <div class="fakeimg" style="height:auto;" class ="responsive"><center>
-		<img src="images/DD-1.png" class ="responsive" alt=""></center>
-	</div>
-    <h1>DR. DHANANJOY DEY</h1>
-    <h4>Associate Professor, Interim Dean (IPR) & Dean (A)</h4>
-    
-  </div>
